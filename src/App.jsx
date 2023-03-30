c6411f7b935881d6ed922fe34f53d6ee47dfeee9
Igor Daniel da Silva <44319834+igor-daniel@users.noreply.github.com>
2023-03-30 15:46:59 -0300

    Componentes criados.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Form } from './components/Form'
+import { Footer } from './components/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
